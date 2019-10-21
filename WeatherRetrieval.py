bca8ead1bed27cdf37e450001bb820a0625762c3
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2019-10-21 14:34:41 +0100

    Dans integration

--- WeatherRetrieval.py
+++ WeatherRetrieval.py
@@ -1,23 +1,23 @@
-import requests
+def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        def LocationToCoors(Location,Country):
+            url= "https://geocode.xyz/"+Location+","+Country+"?json=1" #//https://geocode.xyz/api
+            JSONReturn = requests.get(url).json()
+            LattLong = []
+            LattLong.append(JSONReturn['alt']['loc']['longt'])
+            LattLong.append(JSONReturn['alt']['loc']['latt'])
+            return LattLong
 
-def LocationToCoors(Location,Country):
-    url= "https://geocode.xyz/"+Location+","+Country+"?json=1" #//https://geocode.xyz/api
-    JSONReturn = requests.get(url).json()
-    LattLong = []
-    LattLong.append(JSONReturn['alt']['loc']['longt'])
-    LattLong.append(JSONReturn['alt']['loc']['latt'])
-    return LattLong
-
-
-def WeatherDataRetreival(Loaction,Country):## Location to take format Paris,FR https://darksky.net/poweredby/
-    LattLong = LocationToCoors(Loaction,Country) ## Latitude = 0 ,Longitutde = 1
-    url ="https://api.darksky.net/forecast/60a4da2fceb77dce7d44656141cc6bbf/"+LattLong[1]+","+LattLong[0]
-    print(url)
-    return requests.get(url).json()
-
-def CurrentTemperature(Location,Country):
-    JSONReturn = WeatherDataRetreival(Location,Country)
-    #print(JSONReturn)
-    return JSONReturn["currently"]["temperature"]
-    
-print(CurrentTemperature("Bern","CH"))
+        def WeatherDataRetreival(Loaction,Country):## Location to take format Paris,FR https://darksky.net/poweredby/
+            LattLong = LocationToCoors(Loaction,Country) ## Latitude = 0 ,Longitutde = 1
+            url ="https://api.darksky.net/forecast/60a4da2fceb77dce7d44656141cc6bbf/"+LattLong[1]+","+LattLong[0]
+            print(url)
+            return requests.get(url).json()
+       
+        def CurrentTemperature(Location,Country):
+            JSONReturn = WeatherDataRetreival(Location,Country)
+            #print(JSONReturn)
+            return JSONReturn["currently"]["temperature"]
+           
+        result = (CurrentTemperature("Bern","CH"))
+        speak_output = str(result)
