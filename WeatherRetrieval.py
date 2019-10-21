f43ad2432622267e281198dbd5c13bcec796cc1f
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2019-10-21 10:27:27 +0100

    Can now get current temperature in location

--- WeatherRetrieval.py
+++ WeatherRetrieval.py
@@ -1,15 +1,23 @@
 import requests
 
 def LocationToCoors(Location,Country):
-    url= "https://geocode.xyz/"+Location+Country+"?json=1" #//https://geocode.xyz/api
-    #LattLong = []
-    #return LattLong
-    print (requests.get(url).content)
+    url= "https://geocode.xyz/"+Location+","+Country+"?json=1" #//https://geocode.xyz/api
+    JSONReturn = requests.get(url).json()
+    LattLong = []
+    LattLong.append(JSONReturn['alt']['loc']['longt'])
+    LattLong.append(JSONReturn['alt']['loc']['latt'])
+    return LattLong
 
 
-def WeatherOutput(Loaction,Country):## Location to take format Paris,FR https://darksky.net/poweredby/
+def WeatherDataRetreival(Loaction,Country):## Location to take format Paris,FR https://darksky.net/poweredby/
     LattLong = LocationToCoors(Loaction,Country) ## Latitude = 0 ,Longitutde = 1
-   # url =""
-    #return requests.get(url)
+    url ="https://api.darksky.net/forecast/60a4da2fceb77dce7d44656141cc6bbf/"+LattLong[1]+","+LattLong[0]
+    print(url)
+    return requests.get(url).json()
+
+def CurrentTemperature(Location,Country):
+    JSONReturn = WeatherDataRetreival(Location,Country)
+    #print(JSONReturn)
+    return JSONReturn["currently"]["temperature"]
     
-LocationToCoors("Cologne","DE")
+print(CurrentTemperature("Bern","CH"))
